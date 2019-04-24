4cd02cc71ec428894fa5412048395ef1ade58588
Carlos <carlos.olmedodev@gmail.com>
2019-04-24 14:01:09 +0200

    add: Register function done

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+let config = {
+    host    : 'localhost',
+    user    : 'admin',
+    password: 'admin123',
+    database: 'iris'
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const db = require('mysql');
+const config = require('./config');
+let connection = db.createConnection(config);
+
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+  console.log('Connected to the MySQL server.');
+});
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Home page' });
 });
 
 /* Maps page. */
-router.get('/map', (req, res, next) => {
+router.get('/map', (req, res) => {
   res.render('maps', { title: 'Carte maps' });
 });
 
 /*  Login */
-router.post('/login', (req, res, next) => {
-  res.render('maps', { title: 'Carte maps' });
-
+router.post('/login', (req, res) => {
+  res.render('maps', { title: 'Welcome' });
 });
 
+/*  Register */
+router.post('/register', (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+  let email = req.body.email;
+
+  console.log(`name: ${name} \n pass: ${password} \n email: ${email}`);
+
+  let sql = `
+    INSERT INTO users(name, password, email, travel_time) 
+    VALUES (?, ?, ?, CURRENT_TIMESTAMP)`;
+
+  let values = [name, password, email];
+  //connection.query(sql);
+
+  connection.query(sql, values, (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    console.log(results);
+    res.send('Data saved');
+  });
+
+  connection.end();
+  console.log('Done !');
+
+});
 
 module.exports = router;
